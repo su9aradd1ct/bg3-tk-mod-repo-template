6469adaaa4d69214e5aa3c710def32bee9fb52f6
A <405431+su9aradd1ct@users.noreply.github.com>
2026-01-07 11:20:33 -0800

    docs: update LICENSE

--- repo-setup.py
+++ repo-setup.py
@@ -5,6 +5,19 @@ import argparse
 from pathlib import Path
 
 """
+    https://github.com/su9aradd1ct/bg3-tk-mod-repo-template
+
+    Copyright (c) 2025 xiphiasrex
+    Portions Copyright (c) 2026 su9aradd1ct
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
+"""
     Update these properties if needed.
     Run it from repo directory root with `python repo-setup.py`.
 
